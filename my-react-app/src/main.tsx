6c321ec08b55a0a7c18d54a0be411345bb2e9c7c
Bard <A02242861@usu.edu>
2023-10-09 19:26:34 -0600

    Updated typescript setup, added basic routing test to preview how we will swap between views

--- my-react-app/src/main.tsx
+++ my-react-app/src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.js'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import "./index.css";
+import TestPage0 from "./views/test/TestPage0";
+import TestPage1 from "./views/test/TestPage1";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <TestPage0 />,
+  },
+  {
+    path: "test1",
+    element: <TestPage1 />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
